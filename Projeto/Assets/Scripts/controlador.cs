4c01158d0ec6eb67fc323cca8617391504708647
vitormeds <vitu1221@hotmail.com>
2016-10-06 11:36:12 -0300

    teste Movimentacao
    teste Movimentacao

---
+++ Projeto/Assets/Scripts/controlador.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlador : MonoBehaviour {
+	public Animator animator;
+	public NavMeshAgent agent;
+	public GameObject target;
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		agent.SetDestination (target.transform.position);
+		if (agent.velocity.z>0) {
+			print ("q");
+			animator.SetBool ("inidle", false);
+			animator.SetBool ("inwalk", true);
+		}
+		else if (Input.GetKey (KeyCode.W)) {
+			print ("w");
+			animator.SetBool ("inwalk", false);
+			animator.SetBool ("inidle", true);
+		}
+	}
+}
