5ac53f3b8b68791254365c45a5aabc52c02fbe46
vitormeds <vitu1221@hotmail.com>
2016-10-11 10:34:40 -0300

    Camera Moba
    Camera Moba

--- Projeto/Assets/Scripts/controladorcamera.cs
+++ Projeto/Assets/Scripts/controladorcamera.cs
@@ -10,21 +10,7 @@ public class controladorcamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("w"))
-		{
-			transform.Translate (0,1,0);
-		}
-		if(Input.GetKey("a"))
-		{
-			transform.Translate (-1,0,0);
-		}
-		if(Input.GetKey("s"))
-		{
-			transform.Translate (0,-1,0);
-		}
-		if(Input.GetKey("d"))
-		{
-			transform.Translate (1,0,0);
-		}
+		transform.Translate(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"),0);
+
 }
 }
