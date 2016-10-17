f502379e555103e5688f2fc3ca6919b569fe5bb1
vitormeds <vitu1221@hotmail.com>
2016-10-17 11:57:08 -0200

    Camera e movimentacao
    Camera e movimentacao

---
+++ Projeto/Assets/Scripts/Personagem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Personagem : MonoBehaviour {
+	public Animator animator;
+	public Camera camera;
+	public NavMeshAgent personagem;
+	Vector3 target;
+	// Use this for initialization
+	void Start () {
+		animator.SetBool ("idle", true);
+		animator.SetBool ("walk", false);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) 
+			{
+				target = hit.point;
+				animator.SetBool ("idle", false);
+				animator.SetBool ("walk", true);	
+				personagem.SetDestination (target);
+			}
+		}
+	}
+}
--- Projeto/Assets/Scripts/controladorcamera.cs
+++ Projeto/Assets/Scripts/controladorcamera.cs
@@ -2,15 +2,38 @@
 using System.Collections;
 
 public class controladorcamera : MonoBehaviour {
+	int altura ;
+	int largura;
+	public int m;
 
 	// Use this for initialization
 	void Start () {
-	
+		largura = Screen.width;
+		altura = Screen.height;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"),0);
-
+		print ("X "+Input.mousePosition.x+"Y "+Input.mousePosition.y);
+		if (Input.mousePosition.x == 0) {
+			m = 5;
+			transform.Translate (-m, 0, 0);
+			m = 0;
+		}
+		if (Input.mousePosition.y <= 3) {
+			m = 5;			
+			transform.Translate (0, -m, 0);
+			m = 0;
+		}
+		if (Input.mousePosition.x >= largura-10) {
+			m = 5;
+			transform.Translate (m, 0, 0);
+			m = 0;
+		}
+		if (Input.mousePosition.y >= altura) {
+			m = 5;			
+			transform.Translate (0, m, 0);
+			m = 0;
+		}
 }
 }
