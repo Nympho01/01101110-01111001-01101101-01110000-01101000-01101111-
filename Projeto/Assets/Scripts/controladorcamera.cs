9c74bbfbfb50b3e733f79b8c001d47f914af9a8b
vitormeds <vitu1221@hotmail.com>
2016-10-17 18:51:14 -0200

    Movimentacao e Camera 2
    Movimentacao e Camera 2

--- Projeto/Assets/Scripts/controladorcamera.cs
+++ Projeto/Assets/Scripts/controladorcamera.cs
@@ -5,35 +5,63 @@ public class controladorcamera : MonoBehaviour {
 	int altura ;
 	int largura;
 	public int m;
+	public Quaternion vetor;
+	public Vector3 vetorposicao;
+	bool trava;
 
 	// Use this for initialization
 	void Start () {
 		largura = Screen.width;
 		altura = Screen.height;
+		vetor = transform.rotation;
+		trava = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print ("X "+Input.mousePosition.x+"Y "+Input.mousePosition.y);
-		if (Input.mousePosition.x == 0) {
-			m = 5;
-			transform.Translate (-m, 0, 0);
-			m = 0;
+		if (Input.GetKeyDown("f"))
+		{
+			print ("F");
+			if (trava == true) {
+				trava = false;
+			}
+			else if (trava == false) {
+				trava = true;
+				transform.localPosition = vetorposicao;
+			}
 		}
-		if (Input.mousePosition.y <= 3) {
-			m = 5;			
-			transform.Translate (0, -m, 0);
-			m = 0;
+		if (trava == true) {
+			transform.rotation = vetor;
 		}
-		if (Input.mousePosition.x >= largura-10) {
-			m = 5;
-			transform.Translate (m, 0, 0);
-			m = 0;
-		}
-		if (Input.mousePosition.y >= altura) {
-			m = 5;			
-			transform.Translate (0, m, 0);
-			m = 0;
-		}
+		if (trava == false) {
+			if (transform.localPosition.x >= -23.44f) {
+				if (Input.mousePosition.x == 0) {
+					m = 5;
+					transform.Translate (-m, 0, 0);
+					m = 0;
+				}
+			}
+			if (transform.localPosition.z >= -137.6) {
+				if (Input.mousePosition.y <= 3) {
+					m = 5;			
+					transform.Translate (0, -m, 0);
+					m = 0;
+				}
+			}
+			if (transform.localPosition.x <= 294.3) {
+				if (Input.mousePosition.x >= largura - 10) {
+					m = 5;
+					transform.Translate (m, 0, 0);
+					m = 0;
+				}
+			}
+			if (transform.localPosition.z <= 175.6) {
+				if (Input.mousePosition.y >= altura) {
+					m = 5;			
+					transform.Translate (0, m, 0);
+					m = 0;
+				}
+			}
+			}
 }
 }
