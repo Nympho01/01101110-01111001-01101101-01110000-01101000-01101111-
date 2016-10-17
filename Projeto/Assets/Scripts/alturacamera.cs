9d1e3621a6eaaea5198277b10bf5c9330b242af4
vitormeds <vitu1221@hotmail.com>
2016-10-17 19:56:38 -0200

    Mouse Zoom
    Mouse Zoom

---
+++ Projeto/Assets/Scripts/alturacamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollmouse : MonoBehaviour {
+	int m2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") != 0f) 
+		{
+			m2 = 2;
+			transform.Translate (0,m2,0);
+			m2 = 0;
+		}
+	}
+}
---
+++ Projeto/Assets/Scripts/cameraaltura.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraaltura : MonoBehaviour {
+	int m2,cont;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f) 
+		{
+			if (cont < 15) {
+				m2 = 2;
+				transform.Translate (0, 0, m2);
+				m2 = 0;
+				cont++;
+			}
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0f) 
+		{
+			if (cont != 0) {
+				m2 = 2;
+				transform.Translate (0, 0, -m2);
+				m2 = 0;
+				cont--;
+			}
+		}
+	}
+}
